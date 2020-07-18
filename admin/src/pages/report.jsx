05bc4d3daf2cd8b0409b4446e7ba8c9458c54b87
JASH-JARIWALA <jashjariwala7911@gmail.com>
2020-07-18 19:55:21 +0530

    user report

--- admin/src/pages/report.jsx
+++ admin/src/pages/report.jsx
@@ -1,60 +1,143 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useContext } from 'react';
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import { ToastContainer, toast } from 'react-toastify';
-
+import { AuthContext } from "../data/auth";
+import { Context } from "../data/context";
+import firebase from "../data/firebase";
+import Loading from "../components/LoadingScreen";
 const mapContainerStyle = {
-    width: '50vw',
-    height: '50vh'
+    width: '80vw',
+    height: '70vh'
 }
+
+const defaultcenter = { lat: 21.170240, lng: 72.831062 }
+
 
 const options = {
     disableDefaultUI: true,
     zoomControl: true
 }
 
+const cowAudio = new Audio('/Cow.mp3');
+const goatAudio = new Audio('/Goat.mp3');
 
 export default function Reports() {
+
     const { isLoaded, loadError } = useLoadScript({
         googleMapsApiKey: "AIzaSyDgxTYG7n4gf5qpLdeA_pC_RcTQAc7wdWk"
     })
 
+    const { currentUser } = useContext(AuthContext);
+    const { isLoading, setIsLoading, } = useContext(Context);
+
     const [cattleLocation, setCattleLocation] = useState(null);
     const [userLocation, setUserLocation] = useState(null);
-    const onMapClick = useCallback((event) => {
-        setCattleLocation({
-            lat: event.latLng.lat(),
-            lng: event.latLng.lng()
-        })
-    }, [])
+    const [animalType, setAnimalType] = useState(null);
+
+    const playAudio = () => {
+        if (animalType === "Cow" || animalType === "Buffalo") {
+            cowAudio.load(); cowAudio.play();
+        } else if (animalType === "Goat") {
+            goatAudio.load(); goatAudio.play();
+        }
+    }
+
+    const onMapClick = useCallback(
+        (event) => {
+            setCattleLocation({
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng()
+            })
+            playAudio();
+        },
+        [animalType],
+    )
 
     const [errorText, setErrorText] = useState('');
-    // animalCondition
-    //   animalCount
-    //   animalImageUrl
-    //   animalIsMoving
-    //   animalType
-    //   comments
-    //   coordinates
-    //   userCoords
-    //   createdAt
-    //   description
-    //   uid
+
     const onFormSubmit = (e) => {
         e.preventDefault();
-        const email = e.target.email.value;
+        const animalCondition = e.target.animalCondition.value;
+        const animalCount = e.target.animalCount.value;
+        const animalImage = e.target.animalImageUrl.files[0];
+        const animalIsMoving = e.target.animalIsMoving.checked;
+        const animalType = e.target.animalType.value;
+        const description = e.target.description.value;
+        const email = currentUser.email;
+        if (animalCondition === "Select") {
+            setErrorText("Select animal condition"); return
+        }
+        if (animalCount < 0 || animalCount > 60) {
+            setErrorText("Enter valid animal count"); return
+        }
+        if (!animalImage) {
+            setErrorText("Select image"); return
+        }
+        if (animalType === "Select") {
+            setErrorText("Select animal type"); return
+        }
+        if (description === "") {
+            setErrorText("Enter description"); return
+        }
+        if (!cattleLocation) {
+            setErrorText("Select cattle location"); return
+        }
+        // if (!userLocation) {
+        //     setErrorText("Provide user location"); return
+        // }
+        setIsLoading(true)
+        const uploadTask = firebase.storage().ref('reportImages/').child()
+        uploadTask.put(animalImage).on("state_changed",
+            function (snapshot) {
+                switch (snapshot.state) {
+                    case firebase.storage.TaskState.RUNNING:
+                        console.log("Upload running");
+                        break;
+                    default:
+                        break;
+                }
+            },
+            function (error) {
+                setIsLoading(false);
+                setErrorText(error.message);
+                console.log(error);
+            },
+            () => {
+                uploadTask.getDownloadURL().then((url) => {
+                    firebase.firestore().collection("reports").add({
+                        animalCondition,
+                        animalCount,
+                        animalIsMoving,
+                        animalType,
+                        comments: [],
+                        coordinates: cattleLocation,
+                        userCoords: userLocation,
+                        createdAt: firebase.firestore.Timestamp.now(),
+                        description,
+                        email,
+                        animalImageUrl: url
+                    }).then(() => {
+                        setIsLoading(false);
+                        console.log("completed");
+                    }).catch((error) => { toast.error(error.message) })
+                });
+            }
+        );
+
     }
 
     if (loadError) return "Error Loading maps"
-    if (!isLoaded) return "Loading"
+    if (!isLoaded) return <Loading text="Loading" />
+
+    if (isLoading) return <Loading text="Loading" />
 
 
-  
     let userMarker;
     if (userLocation) {
-        if (!(userLocation === cattleLocation)) {            
+        if (!(userLocation === cattleLocation)) {
             userMarker = (
-                <Marker 
+                <Marker
                     icon={{
                         url: '/location.svg',
                         scaledSize: new window.google.maps.Size(30, 30)
@@ -66,9 +149,13 @@ export default function Reports() {
     }
     let cattleMarker;
     if (cattleLocation) {
+        let url = '/location-pin.svg';
+        url = animalType === "Cow" ? '/cow.svg' : url;
+        url = animalType === "Buffalo" ? '/buffalo.svg' : url;
+        url = animalType === "Goat" ? '/goat.svg' : url;
         cattleMarker = (
             <Marker icon={{
-                url: '/cow.svg',
+                url,
                 scaledSize: new window.google.maps.Size(30, 30)
             }}
                 position={cattleLocation}
@@ -90,7 +177,7 @@ export default function Reports() {
             }, (error) => {
                 toast.error("error"); console.log(error);
             }, { maximumAge: 60000, timeout: 5000, enableHighAccuracy: true }
-            );            
+            );
         } else {
             toast.error("Function not supported")
         }
@@ -105,7 +192,7 @@ export default function Reports() {
             }, (error) => {
                 toast.error("error"); console.log(error);
             }, { maximumAge: 60000, timeout: 5000, enableHighAccuracy: true }
-            );            
+            );
         } else {
             toast.error("Function not supported")
         }
@@ -115,10 +202,11 @@ export default function Reports() {
 
         <Container className="justify-content-md-center">
             <h1>Report Cattle</h1>
-            <Form onSubmit={(e) => onFormSubmit(e)}>
+            <Form onSubmit={(e) => onFormSubmit(e)} className="col-lg-6">
                 <Form.Group controlId="animalCondition">
                     <Form.Label>Animal Condition</Form.Label>
                     <Form.Control as="select">
+                        <option>Select</option>
                         <option>Death</option>
                         <option>Injured</option>
                         <option>Normal</option>
@@ -126,21 +214,28 @@ export default function Reports() {
                 </Form.Group>
                 <Form.Group controlId="animalCount">
                     <Form.Label>Animal Count</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
+                    <Form.Control required type="number" placeholder="Approximate animal count" />
                 </Form.Group>
-                <Form>
-                    <Form.Group>
-                        <Form.File id="animalImageUrl" label="Animal Image" />
-                    </Form.Group>
-                </Form>
-                <Form.Check
-                    type="switch"
-                    id="animalIsMoving"
-                    label="Animal is moving"
-                />
+                <Form.Group>
+                    <Form.File id="animalImageUrl" max={1} label="Animal Image" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label >
+                        Status
+                    </Form.Label>
+
+                    <Form.Check
+                        type="checkbox"
+                        label="moving"
+                        name="animalIsMoving"
+                        id="animalMoving1"
+                    />
+                </Form.Group>
+
                 <Form.Group controlId="animalType">
                     <Form.Label>Animal Type</Form.Label>
-                    <Form.Control as="select">
+                    <Form.Control as="select" onChange={(e) => setAnimalType(e.target.value)}>
+                        <option>Select</option>
                         <option>Cow</option>
                         <option>Buffalo</option>
                         <option>Goat</option>
@@ -160,7 +255,7 @@ export default function Reports() {
                     <GoogleMap
                         mapContainerStyle={mapContainerStyle}
                         zoom={15}
-                        center={cattleLocation ? cattleLocation : { lat: 21.170240, lng: 72.831062 }}
+                        center={defaultcenter}
                         options={options}
                         onClick={onMapClick}
                     >
