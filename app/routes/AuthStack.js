58cf40caab7068d26dbf59aa080cda4c9ee4a7de
Ferin <ferin@localhost.localdomain>
2020-07-29 17:16:12 +0530

    Intro Screen Added

--- app/routes/AuthStack.js
+++ app/routes/AuthStack.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import SignIn from "../auth/SignIn";
+import Intro from "../auth/Intro";
+import LocateReport from "../auth/LocateReport";
+import Votes from "../auth/Votes";
 
 const AuthStackScreen = createStackNavigator();
 
 const AuthStack = () => {
   return (
-    <AuthStackScreen.Navigator>
+    <AuthStackScreen.Navigator initialRouteName="Intro">
       <AuthStackScreen.Screen
         component={SignIn}
         name="SignIn"
+        options={{ header: () => null }}
+      />
+      <AuthStackScreen.Screen
+        component={Intro}
+        name="Intro"
+        options={{ header: () => null }}
+      />
+      <AuthStackScreen.Screen
+        component={LocateReport}
+        name="LocateReport"
+        options={{ header: () => null }}
+      />
+      <AuthStackScreen.Screen
+        component={Votes}
+        name="VoteReview"
         options={{ header: () => null }}
       />
     </AuthStackScreen.Navigator>
